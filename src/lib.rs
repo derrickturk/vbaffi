07d121d4e5dc21ccf4813ee885b620596fb93246
Derrick W. Turk <dwt@terminusdatascience.com>
2021-09-27 14:25:19 -0500

    array of UDTs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ptr, slice};
+use std::{mem, ptr, slice};
 
 use winapi::{
     um::{
@@ -27,7 +27,38 @@ pub struct ExampleUDT {
 #[no_mangle]
 pub unsafe extern "system"
 fn sum_values(udt: *const ExampleUDT) -> f64 {
-    let values = (*udt).values;
+    sum_values_impl(&*udt)
+}
+
+#[no_mangle]
+pub unsafe extern "system"
+fn hypersum_values(udts: *mut LPSAFEARRAY) -> f64 {
+    let udts = *udts;
+    let mut val_buf: *const ExampleUDT = ptr::null();
+
+    if SafeArrayAccessData(udts, &mut val_buf as *mut _ as *mut _) != S_OK {
+        return 1.0;
+    }
+
+    let _guard = Guard::new(|| { SafeArrayUnaccessData(udts); });
+
+    if (*udts).cDims != 1 {
+        return 0.0;
+    }
+
+    if (*udts).cbElements as usize != mem::size_of::<ExampleUDT>() {
+        return 0.0;
+    }
+
+    let n = (*udts).rgsabound[0].cElements as usize;
+    let values = slice::from_raw_parts(val_buf, n);
+
+    values.iter().map(|udt| sum_values_impl(udt)).sum()
+}
+
+#[inline]
+unsafe fn sum_values_impl(udt: &ExampleUDT) -> f64 {
+    let values = udt.values;
     let mut val_buf: *const f64 = ptr::null();
 
     if SafeArrayAccessData(values, &mut val_buf as *mut _ as *mut _) != S_OK {
@@ -40,7 +71,7 @@ fn sum_values(udt: *const ExampleUDT) -> f64 {
         return 0.0;
     }
 
-    if (*values).cbElements != 8  {
+    if (*values).cbElements != 8 {
         return 0.0;
     }
 
